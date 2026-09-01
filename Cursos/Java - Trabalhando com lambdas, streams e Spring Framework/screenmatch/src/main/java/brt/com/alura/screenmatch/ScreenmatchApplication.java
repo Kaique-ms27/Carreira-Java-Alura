bb93437d46f9a6bb099e81308b6d7ef991860659
Kaique Magalhães Santos <kaiquemagalhaes27@gmail.com>
2026-09-01 19:38:16 -0300

    Criação da classe record DadosEpisodios, para salvar as informações dos epsódios

--- Cursos/Java - Trabalhando com lambdas, streams e Spring Framework/screenmatch/src/main/java/brt/com/alura/screenmatch/ScreenmatchApplication.java
+++ Cursos/Java - Trabalhando com lambdas, streams e Spring Framework/screenmatch/src/main/java/brt/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,5 +1,6 @@
 package brt.com.alura.screenmatch;
 
+import brt.com.alura.screenmatch.model.DadosEpisodios;
 import brt.com.alura.screenmatch.model.DadosSeries;
 import brt.com.alura.screenmatch.service.ConsumoAPI;
 import brt.com.alura.screenmatch.service.ConverteDados;
@@ -19,15 +20,22 @@ public class ScreenmatchApplication implements CommandLineRunner {
 		ConverteDados conversor = new ConverteDados();
 
 		// Endereço da API do IMDB
-		String endereco = "http://www.omdbapi.com/?t=Suits&apikey=4e39c79e";
+		String enderecoSerie = "http://www.omdbapi.com/?t=Suits&apikey=4e39c79e";
 		var consumoAPI = new ConsumoAPI();
 
 		// Consumindo os dados da API
-		var json = consumoAPI.obterDados(endereco);
+		var json = consumoAPI.obterDados(enderecoSerie);
 
-		// Converte os dados do json para apenas os dados da classe converteDados
-		DadosSeries dados = conversor.obterDados(json, DadosSeries.class);
+		// Converte os dados do json para apenas os dados da classe DadosSeries
+		DadosSeries dadosSeries = conversor.obterDados(json, DadosSeries.class);
 		// Exibir os dados
-		System.out.println(dados);
+		System.out.println(dadosSeries);
+
+
+		// Convertendo os dados para a classe DadosEpisodios
+		String enderecoEpisodio = "http://www.omdbapi.com/?t=suits&season=1&episode=1&apikey=4e39c79e";
+		json = consumoAPI.obterDados(enderecoEpisodio);
+		DadosEpisodios dadosEpisodios = conversor.obterDados(json, DadosEpisodios.class);
+		System.out.println(dadosEpisodios);
 	}
 }
---
+++ Cursos/Java - Trabalhando com lambdas, streams e Spring Framework/screenmatch/src/main/java/brt/com/alura/screenmatch/model/DadosEpisodios.java
@@ -0,0 +1,11 @@
+package brt.com.alura.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosEpisodios(@JsonAlias("Title") String title,
+                             @JsonAlias("Episode") int episode,
+                             @JsonAlias("imdbRating") String rating,
+                             @JsonAlias("Released") String released) {
+}
